5ee1f637ed03e25ee99fda7e8abaa846f14fbea0
N.Mikhaylov <nmikhaylov92@gmail.com>
2019-05-07 15:05:42 +0300

    Project Step 2

--- js/main.js
+++ js/main.js
@@ -17,5 +17,29 @@ for (let i = 0; i < 2; i += 1) {
   appData.expenses[name] = money;
 }
 
+/* 
+let i = 0;
+while (i < 2) {
+  const name = prompt('Введите обязательную статью расходов в этом месяце');
+  const money = prompt('Во сколько обойдется?', 0);
+
+  appData.expenses[name] = money;
+  i += 1;
+}
+*/
+
+/* 
+let i = 0;
+
+do {
+  const name = prompt('Введите обязательную статью расходов в этом месяце');
+  const money = prompt('Во сколько обойдется?', 0);
+
+  appData.expenses[name] = money;
+  i += 1;
+} while (i < 2);
+
+*/
+
 alert(appData.budget / 30);
 console.log(appData);
